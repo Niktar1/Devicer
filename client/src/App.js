e78969674610cb2d9c0a337d21bbddabf8d485cb
Nik <com.nik570@gmail.com>
2024-11-05 02:29:03 +0200

    imported files from old client side in this project

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// import Users from './components/Users';
+import { createBrowserRouter, Link, RouterProvider} from "react-router-dom";
+
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Link to={'/users'}>Hello</Link>,
+    },
+    // {
+    //   path: "/users",
+    //   element: <Users />,
+    // },
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <RouterProvider router={router} />
+      </div>
+      
     </div>
   );
 }
---
+++ client/src/components/Users.js
@@ -0,0 +1,29 @@
+// src/components/Users.js
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const Users = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const response = await axios.get('http://localhost:5000/users'); // NestJS API URL
+      setUsers(response.data);
+    };
+
+    fetchUsers();
+  }, []);
+
+  return (
+    <div>
+      <h1>Users</h1>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>{user.email}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Users;
